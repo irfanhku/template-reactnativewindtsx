2b2a5f86e780f887d549804cabf735897af2e8f5
irfanx <irfan.ulya@digitalbdg.ac.id>
2026-05-22 09:37:12 +0700

    fix nativewind

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import './global.css'
+import './global.css';
 import 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
